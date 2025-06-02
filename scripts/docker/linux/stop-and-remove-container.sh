86744387919f5fb2c86aca77b76caf06a8297c09
Andrea Borgogelli Avveduti <borgogelli@iubar.it>
2025-06-02 19:38:00 +0200

    .

--- scripts/docker/linux/stop-and-remove-container.sh
+++ scripts/docker/linux/stop-and-remove-container.sh
@@ -14,13 +14,16 @@ source "$SCRIPT_DIR/config.sh"
 # Se vuoi chiedere all'utente di inserire manualmente un nome o ID del container:
 # read -p "Inserisci il nome o ID del container da fermare ed eliminare: " CONTAINER_NAME
 
+ERROR=0
+
 # Ferma il container
 echo " 🐳 Fermo il container: $CONTAINER_NAME..."
 docker stop "$CONTAINER_NAME"
 
 if [ $? -ne 0 ]; then
-    echo "❌ Il comando è fallito!"
-    exit 1
+    echo "❌ Il comando \"stop\" è fallito!"
+    echo "(🤔 Forse il container è già stato fermato via cli ? )"
+    (( ERROR++ ))
 else
     echo "...OK ✅"
 fi
@@ -30,10 +33,23 @@ echo "🐳 Elimino il container: $CONTAINER_NAME..."
 docker rm "$CONTAINER_NAME"
 
 if [ $? -ne 0 ]; then
-    echo "❌ Il comando è fallito!"
-    exit 1
+    echo "❌ Il comando \"rm\" è fallito!"
+    echo "(🤔 Forse il container è già stato eliminato via cli ?)"
+    (( ERROR++ ))
 else
     echo "...OK ✅"
+fi
+
+if [ "$ERROR" -gt 0 ]; then
+    echo "Si sono verificati uno o più errori."
+    read -p "Vuoi continuare lo stesso ? (s/n): " answer    
+    if [[ "$answer" =~ ^[Ss]$ ]]; then    
+        echo "✅ Procedo..."
+    else
+        echo "❌ Operazione annullata."
+        exit 1
+    fi  
+    else
 fi
 
 # Pausa opzionale (simile a PAUSE in batch)
