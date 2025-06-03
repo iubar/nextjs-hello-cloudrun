628bfc3840a5de592878c6aa6cc7f93839bb50ce
Andrea Borgogelli Avveduti <borgogelli@iubar.it>
2025-06-03 09:10:03 +0200

    [skip ci]

--- scripts/deploy/cloudrun.sh
+++
@@ -1,7 +0,0 @@
-gcloud builds submit --tag gcr.io/PROJECT-ID/nextjs-app
-gcloud run deploy nextjs-app \
-  --image gcr.io/PROJECT-ID/nextjs-app \
-  --platform managed \
-  --region europe-west1 \
-  --allow-unauthenticated \
-  --port 8080
